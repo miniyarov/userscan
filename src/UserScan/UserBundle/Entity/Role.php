c1e58a97e0e6794bbc03542de01efa14406b1cd4
Ulugbek Miniyarov <macbook@Macbooks-MacBook.local>
2012-10-14 19:09:06 +0300

    simple bug fix

--- src/UserScan/UserBundle/Entity/Role.php
+++ src/UserScan/UserBundle/Entity/Role.php
@@ -21,7 +21,7 @@ class Role implements RoleInterface
     protected $id;
 
     /**
-    * @ORM\Column(type="string", length="255")
+    * @ORM\Column(type="string", length=255)
     *
     * @var string $name
     */
